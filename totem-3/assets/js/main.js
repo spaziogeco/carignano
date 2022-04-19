4f412e498955656541c046370080ed62e97975ba
Mirkofilip <99646568+Mirkofilip@users.noreply.github.com>
2022-04-19 20:05:54 +0200

    totem-3

---
+++ totem-3/assets/js/main.js
@@ -0,0 +1,214 @@
+jQuery(document).ready(function($){
+  AOS.init();
+  $('.box').removeClass('aos-animate');
+  $('.load-video .btn-play').on('click',function(){
+    document.getElementById('vid-intro').play();
+    $('.load-video').addClass('none');
+    $('.start-game').addClass('on');
+  });
+  $('span.start-game').on('click',function(){
+    $('.box').addClass('aos-animate');
+    const videoIntro = document.getElementById('vid-intro');
+    videoIntro.pause();
+    videoIntro.currentTime = 0;
+    $('.intro-video').slideToggle().addClass('none');
+    $('.container-answer video').attr('src','assets/video/QUIZ RISORGIMENTO 3 X/DOMANDA 1 RISORGIMENTO.mp4');
+    $('.start-game').removeClass('on');
+  });
+  const boxH = $('.box').outerWidth();
+  $('.box').css('height',boxH);
+  $('.box').on('click',function(){
+    const video_answer = document.getElementById('vid');
+    video_answer.pause();
+    video_answer.currentTime = 0;
+    $('.box,.animation-bg span,.container-reply h2,.img-reply,.next_answer a,.back').removeClass('aos-animate');
+    setTimeout(function() {
+        $('.container-reply h2,.animation-bg span,.img-reply,.next_answer a,.back').addClass('aos-animate');
+    }, 400);
+    const data_value = $(this).attr('data-success');
+    const color = $(this).attr('data-color');
+    if (data_value == 'true') {
+        console.log('rispsota giusta');
+        $('.container-reply .container-success').css('background', color);
+        $('.animation-bg.star').addClass('visible')
+        $('.success-reply').addClass('visible');
+        $('.failure-reply').removeClass('visible');
+        $('.animation-bg.cloud').removeClass('visible');
+        document.getElementById('vid-jump').play();
+    }else{
+      console.log('risposta sbagliata');
+      $('.container-reply .container-failure').css('background', color);
+      $('.animation-bg.star').removeClass('visible')
+      $('.animation-bg.cloud').addClass('visible')
+      $('.success-reply').removeClass('visible');
+      $('.failure-reply').addClass('visible');
+      document.getElementById('vid-triste').play();
+    }
+    $('.container-reply').addClass('activate');
+    // setTimeout(function(){
+    //   $('.load-video').removeClass('none');
+    //   $('.success-reply,.failure-reply').removeClass('visible');
+    //   $('.container-reply.activate').removeClass('activate');
+    //   $('.failure-reply,.success-reply').removeClass('activate');
+    // },10000);
+  });
+
+    //Domande
+    let domanda_1 = 'Quale antico stato è rappresentato nella cartina?';
+    let domanda_2 = 'Chi è il primo re di Italia?';
+    let domanda_3 = 'Guardando la cartina in che regno si trovava Carignano?';
+    let domanda_4 = 'domanda 4';
+    let video_1 = "assets/video/QUIZ RISORGIMENTO 3 X/DOMANDA 1 RISORGIMENTO.mp4";
+    let video_2 = "assets/video/QUIZ RISORGIMENTO 3 X/DOMANDA 2 RISORGIMENTO.mp4";
+    let video_3 = "assets/video/QUIZ RISORGIMENTO 3 X/DOMANDA 3 RISORGIMENTO.mp4";
+    let r_1 = 'Lombardo-Veneto';
+    let r_2 = 'Stato pontificio';
+    let r_3 = 'Regno delle due Sicilie';
+    let r_4 = 'Regno di Sardegna';
+    let r_5 = 'Carlo Alberto';
+    let r_6 = 'Carlo Martello';
+    let r_7 = 'Vittorio Emanuele II';
+    let r_8 = 'Luigi XIV';
+    let r_9 = 'Regno delle due Sicilie';
+    let r_10 = 'Regno di Sardegna';
+    let r_11 = 'Impero asburgico';
+    let r_12 = 'Stato pontificio';
+
+
+    $('.back').on('click',function(){
+      AOS.init();
+      $('.success-reply,.failure-reply').removeClass('visible');
+      $('.container-reply.activate').removeClass('activate');
+      $('.failure-reply,.success-reply').removeClass('activate');
+      const video_bad = document.getElementById('vid-triste');
+      video_bad.pause();
+      video_bad.currentTime = 0;
+      const video_answer = document.getElementById('vid');
+      video_answer.play();
+      video_answer.currentTime = 0;
+    });
+
+    $('.next-start').on('click', function(){
+      if ($(this).hasClass('next-start')) {
+        AOS.init();
+        $(this).removeClass().addClass('next-2');
+        $('.answer h2').text(domanda_2);
+        $('.box:first-child').attr('data-success','false').find('h3').text(r_5);
+        $('.box:first-child').find('img').attr('src','assets/img/5.png');
+        $('.box:nth-child(2)').attr('data-success','false').find('h3').text(r_6);
+        $('.box:nth-child(2)').find('img').attr('src','assets/img/6.png');
+        $('.box:nth-child(3)').attr('data-success','true').find('h3').text(r_7);
+        $('.box:nth-child(3)').find('img').attr('src','assets/img/7.png')
+        $('.box:nth-child(4)').attr('data-success','false').find('h3').text(r_8);
+        $('.box:nth-child(4)').find('img').attr('src','assets/img/8.png');
+        $(this).hide();
+        $('.next-2').show();
+        $('.success-reply,.failure-reply').removeClass('visible');
+        $('.container-reply.activate').removeClass('activate');
+        $('.failure-reply,.success-reply').removeClass('activate');
+        $('.container-answer video').attr('src',video_2);
+        const video_jump = document.getElementById('vid-jump');
+        video_jump.pause();
+        video_jump.currentTime = 0;
+        setTimeout(function() {
+            $('.container-reply h2,.animation-bg span,.img-reply,.next_answer a').addClass('aos-animate');
+        }, 400);
+      }else if ($(this).hasClass('next')) {
+        AOS.init();
+        $(this).removeClass().addClass('next-1');
+        $('.box:first-child').attr('data-success','true').find('h3').text(r_1);
+        $('.box:first-child').find('img').attr('src','assets/img/1.png');
+        $('.box:nth-child(2)').attr('data-success','false').find('h3').text(r_2);
+        $('.box:nth-child(2)').find('img').attr('src','assets/img/2.png');
+        $('.box:nth-child(3)').attr('data-success','false').find('h3').text(r_3);
+        $('.box:nth-child(3)').find('img').attr('src','assets/img/3.png')
+        $('.box:nth-child(4)').attr('data-success','false').find('h3').text(r_4);
+        $('.box:nth-child(4)').find('img').attr('src','assets/img/4.png');
+        $('.success-reply,.failure-reply').removeClass('visible');
+        $('.container-reply.activate').removeClass('activate');
+        $('.failure-reply,.success-reply').removeClass('activate');
+        $('.answer h2').text(domanda_1);
+        const video_jump = document.getElementById('vid-jump');
+        video_jump.pause();
+        video_jump.currentTime = 0;
+        setTimeout(function() {
+            $('.container-reply h2,.animation-bg span').addClass('aos-animate');
+        }, 400);
+      }else if($(this).hasClass('next-1')){
+        AOS.init();
+        $(this).removeClass().addClass('next-2');
+        $('.answer h2').text(domanda_2);
+        $('.box:first-child').attr('data-success','false').find('h3').text(r_5);
+        $('.box:first-child').find('img').attr('src','assets/img/5.png');
+        $('.box:nth-child(2)').attr('data-success','false').find('h3').text(r_6);
+        $('.box:nth-child(2)').find('img').attr('src','assets/img/6.png');
+        $('.box:nth-child(3)').attr('data-success','true').find('h3').text(r_7);
+        $('.box:nth-child(3)').find('img').attr('src','assets/img/7.png')
+        $('.box:nth-child(4)').attr('data-success','false').find('h3').text(r_8);
+        $('.box:nth-child(4)').find('img').attr('src','assets/img/8.png');
+        $(this).hide();
+        $('.next-2').show();
+        $('.success-reply,.failure-reply').removeClass('visible');
+        $('.container-reply.activate').removeClass('activate');
+        $('.failure-reply,.success-reply').removeClass('activate');
+        $('.container-answer video').attr('src',video_2);
+        const video_jump = document.getElementById('vid-jump');
+        video_jump.pause();
+        video_jump.currentTime = 0;
+        setTimeout(function() {
+            $('.container-reply h2,.animation-bg span,.img-reply,.next_answer a').addClass('aos-animate');
+        }, 400);
+      }else if($(this).hasClass('next-2')){
+        AOS.init();
+        $(this).removeClass().addClass('next-final');
+        $('.answer h2').text(domanda_3);
+        $('.box:first-child').attr('data-success','false').find('h3').text(r_9);
+        $('.box:first-child').find('img').attr('src','assets/img/9.png');
+        $('.box:nth-child(2)').attr('data-success','true').find('h3').text(r_10);
+        $('.box:nth-child(2)').find('img').attr('src','assets/img/10.png');
+        $('.box:nth-child(3)').attr('data-success','false').find('h3').text(r_11);
+        $('.box:nth-child(3)').find('img').attr('src','assets/img/11.png')
+        $('.box:nth-child(4)').attr('data-success','false').find('h3').text(r_12);
+        $('.box:nth-child(4)').find('img').attr('src','assets/img/12.png');
+        $('.success-reply,.failure-reply').removeClass('visible');
+        $('.container-reply.activate').removeClass('activate');
+        $('.failure-reply,.success-reply').removeClass('activate');
+        $('.container-answer video').attr('src',video_3);
+        const video_jump = document.getElementById('vid-jump');
+        video_jump.pause();
+        video_jump.currentTime = 0;
+      }else if($(this).hasClass('next-final')){
+        AOS.init();
+        $('.next-totem').addClass('visible');
+        setTimeout(function(){
+          $('.next-totem').removeClass('visible');
+        },5000);
+        $(this).removeClass().addClass('next-1');
+        $('.answer h2').text(domanda_1);
+        $('.box:first-child').attr('data-success','true').find('h3').text(r_1);
+        $('.box:first-child').find('img').attr('src','assets/img/1.png');
+        $('.box:nth-child(2)').attr('data-success','false').find('h3').text(r_2);
+        $('.box:nth-child(2)').find('img').attr('src','assets/img/2.png');
+        $('.box:nth-child(3)').attr('data-success','false').find('h3').text(r_3);
+        $('.box:nth-child(3)').find('img').attr('src','assets/img/3.png')
+        $('.box:nth-child(4)').attr('data-success','false').find('h3').text(r_4);
+        $('.box:nth-child(4)').find('img').attr('src','assets/img/4.png');
+        setTimeout(function(){
+          $('.load-video').removeClass('none');
+          $('.intro-video').slideToggle().removeClass('none');
+          $('.success-reply,.failure-reply').removeClass('visible');
+          $('.container-reply.activate').removeClass('activate');
+          $('.failure-reply,.success-reply').removeClass('activate');
+        },500);
+        $('.container-answer video').attr('src',video_1);
+        const video_jump = document.getElementById('vid-jump');
+        video_jump.pause();
+        video_jump.currentTime = 0;
+        const video_answer = document.getElementById('vid');
+        video_answer.pause();
+        video_answer.currentTime = 0;
+      }
+    })
+
+
+});
